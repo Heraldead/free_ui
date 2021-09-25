d22dc3d4eecf4837cbdf2a096d1060b0017fd6c6
Mick <Lord-androv@mail.ru>
2021-09-25 03:14:39 +0300

    new

---
+++ lib/main.dart
@@ -0,0 +1,219 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomPaint(
+        painter: BackgroundSignIn(),
+        child: Stack(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: <Widget>[
+                  _getHeader(),
+                  _getInputs(),
+                  _getSignIn(),
+                  SizedBox(
+                    height: 70,
+                  )
+                  // _getBottomRow(),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+_getHeader() {
+  return Expanded(
+    flex: 3,
+    child: Container(
+      alignment: Alignment.bottomLeft,
+      child: Text(
+        'Добро пожаловать',
+        style: TextStyle(color: Colors.white, fontSize: 35),
+      ),
+    ),
+  );
+}
+
+_getInputs() {
+  return Expanded(
+    flex: 4,
+    child: Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Логин',
+            hintStyle: TextStyle(color: Colors.white),
+            enabledBorder: new UnderlineInputBorder(
+              borderSide: BorderSide(
+                  color: Colors.white, width: 1.0, style: BorderStyle.solid),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+        TextField(
+          decoration: InputDecoration(
+            hintText: 'Пароль',
+            hintStyle: TextStyle(color: Colors.white),
+            enabledBorder: new UnderlineInputBorder(
+              borderSide: BorderSide(
+                  color: Colors.white, width: 1.0, style: BorderStyle.solid),
+            ),
+          ),
+        ),
+        SizedBox(
+          height: 15,
+        ),
+      ],
+    ),
+  );
+}
+
+_getSignIn() {
+  return Expanded(
+    flex: 1,
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(
+          'Вход',
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.white),
+        ),
+        Button()
+      ],
+    ),
+  );
+}
+
+class Button extends StatefulWidget {
+  const Button({Key key}) : super(key: key);
+
+  @override
+  _ButtonState createState() => _ButtonState();
+}
+
+class _ButtonState extends State<Button> {
+  second_widget() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 70,
+      height: 70,
+      child: ElevatedButton(
+        onPressed: second_widget,
+        style: ButtonStyle(
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                    side: BorderSide(color: Colors.blue)))),
+        child: Icon(
+          Icons.arrow_forward_ios_rounded,
+          size: 30,
+        ),
+      ),
+    );
+  }
+}
+
+// _getBottomRow() {
+//   return Expanded(
+//     flex: 1,
+//     child: Row(
+//       mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//       children: <Widget>[
+//         Text(
+//           'Регистрация',
+//           style: TextStyle(
+//               fontSize: 15,
+//               fontWeight: FontWeight.w500,
+//               decoration: TextDecoration.underline),
+//         ),
+//         Text(
+//           'Забыли пароль',
+//           style: TextStyle(
+//               fontSize: 15,
+//               fontWeight: FontWeight.w500,
+//               decoration: TextDecoration.underline),
+//         )
+//       ],
+//     ),
+//   );
+// }
+
+class BackgroundSignIn extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {
+    var sw = size.width;
+    var sh = size.height;
+    var paint = Paint();
+
+    Path mainBackground = Path();
+    mainBackground.addRect(Rect.fromLTRB(0, 0, sw, sh));
+    paint.color = Colors.grey[900];
+    canvas.drawPath(mainBackground, paint);
+
+    // Blue
+    Path blueWave = Path();
+    blueWave.lineTo(sw, 0);
+    blueWave.lineTo(sw, sh * 0.5);
+    blueWave.quadraticBezierTo(sw * 0.5, sh * 0.45, sw * 0.2, 0);
+    blueWave.close();
+    paint.color = Colors.red;
+    canvas.drawPath(blueWave, paint);
+
+    // Grey
+    Path greyWave = Path();
+    greyWave.lineTo(sw, 0);
+    greyWave.lineTo(sw, sh * 0.1);
+    greyWave.cubicTo(
+        sw * 0.95, sh * 0.15, sw * 0.65, sh * 0.15, sw * 0.6, sh * 0.38);
+    greyWave.cubicTo(sw * 0.52, sh * 0.52, sw * 0.05, sh * 0.45, 0, sh * 0.4);
+    greyWave.close();
+    paint.color = Colors.blue;
+    canvas.drawPath(greyWave, paint);
+
+    // Yellow
+    Path yellowWave = Path();
+    yellowWave.lineTo(sw * 0.7, 0);
+    yellowWave.cubicTo(
+        sw * 0.6, sh * 0.05, sw * 0.27, sh * 0.01, sw * 0.18, sh * 0.12);
+    yellowWave.quadraticBezierTo(sw * 0.12, sh * 0.2, 0, sh * 0.2);
+    yellowWave.close();
+    paint.color = Colors.white;
+    canvas.drawPath(yellowWave, paint);
+  }
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return oldDelegate != this;
+  }
+}
